3bba791e9b523ca76be915f6998cac71183b3298
LucasCarvalho <lucas.carvalho7933@gmail.com>
2025-04-25 13:56:00 -0300

    ajuste na logica

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,17 @@
 let calculateIMC = () => {
-    let weight = Number(document.getElementById('weight').value)
-    let height = Number(document.getElementById('height').value)
+    const weight = Number(document.getElementById('weight').value)
+    const height = Number(document.getElementById('height').value)
 
     heightSquared = Math.pow(height, 2);
     result = (weight / heightSquared).toFixed(2);
 
-    let resultIMC = document.getElementById('imc-value')
-    let resultArea = document.getElementById('result-area').style.display = 'block';
+    const resultIMC = document.getElementById('imc-value')
+    let resultArea = document.getElementById('result-area');
+    resultArea.style.display = 'block';
+    resultArea.style.transition
     return resultIMC.innerText = result
 }
 
-let calculate = document.getElementById('calculate')
+const calculate = document.getElementById('calculate')
 calculate.addEventListener('click', calculateIMC)
 
